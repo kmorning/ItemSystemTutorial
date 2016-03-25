5b4f2692adc78f92ceca56db598ed2d134b99a26
Kelly Morning <kleemorning@gmail.com>
2016-03-25 00:58:25 -0400

    Initial commit

---
+++ Assets/Altered Incarnation/Item System/Scripts/Editor/ISObjectEditor/ISObjectEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+namespace AlteredIncarnation.ItemSystem.Editor
+{
+    public partial class ISObjectEditor : EditorWindow
+    {
+        [MenuItem("AltInc/Database/Item Editor %#i")]
+        public static void Init()
+        {
+            ISObjectEditor window = EditorWindow.GetWindow<ISObjectEditor>();
+            window.minSize = new Vector2(800, 600);
+
+            // TODO:  Icon/tooltip
+            window.titleContent.text = "Item DB";
+
+            window.Show();
+        }
+
+        void OnEnable()
+        {
+        }
+
+        void OnGUI
+        {
+        }
+    }
+}
