e77d14e1673073fd0762b4b37da87e244977a887
Kelly Morning <kleemorning@gmail.com>
2016-03-25 01:11:26 -0400

    Initial commit

---
+++ Assets/Altered Incarnation/Item System/Scripts/Editor/ISObjectEditor/TopTabBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AlteredIncarnation.ItemSystem.Editor
+{
+    public partial class ISObjectEditor
+    {
+        void TopTabBar()
+        {
+            GUILayout.BeginHorizontal("Box", GUILayout.ExpandWidth(true));
+            GUILayout.Button("Weapons");
+            GUILayout.EndHorizontal();
+        }
+    }
+}
