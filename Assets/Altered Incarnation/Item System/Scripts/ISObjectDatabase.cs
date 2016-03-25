786494a588a4ec354b65f6c75b03838c33675fc3
Kelly Morning <kleemorning@gmail.com>
2016-03-25 00:33:22 -0400

    Implement concrete ScriptableObjectDatabase class

--- Assets/Altered Incarnation/Item System/Scripts/ISObjectDatabase.cs
+++ Assets/Altered Incarnation/Item System/Scripts/ISObjectDatabase.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 namespace AlteredIncarnation.ItemSystem
 {
-    public class ISObjectDatabase : ScriptableObject
+    public class ISObjectDatabase : ScriptableObjectDatabase<ISObject>
     {
+        public static ISObjectDatabase LoadDatabase(string dbPath, string dbName)
+        {
+            return LoadDatabase<ISObjectDatabase>(dbPath, dbName);
+        }
     }
 }
